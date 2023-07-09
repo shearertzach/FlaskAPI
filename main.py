871d69db1e14fce2ace420c9a988e6a0c518d6a1
Zach Shearer <zachshearer@zachs-mbp.lan>
2023-07-09 09:18:36 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,32 @@
+from flask import Flask
+import sqlite3
+import random
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+  db = sqlite3.connect('testdatabase.db')
+  cursor = db.cursor()
+
+  for _ in range(1, 10):
+      username = f"user{random.randint(1000000, 9999999)}"
+      password = 'testpword'
+
+      cursor.execute(
+          "INSERT INTO users (username, password) VALUES (?, ?)", (username, password))
+      print(f"Adding {username} to insert statement.")
+
+  print(f"Committing users to database.")
+
+  db.commit()
+
+  return '<h1>Added users to database</h1>'
+
+
+
+
+if __name__ == '__main__':
+    app.run()
